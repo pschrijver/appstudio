425437723a0d55e2741fe759b94563759219cc9c
Paul <schrijver.paul@gmail.com>
2015-05-18 14:21:47 +0200

    Classes update

--- GhostProject/Ghost/app/src/main/java/com/example/paulschrijver/ghost/Dictionary.java
+++ GhostProject/Ghost/app/src/main/java/com/example/paulschrijver/ghost/Dictionary.java
@@ -1,35 +1,87 @@
 package com.example.paulschrijver.ghost;
 
+import java.util.ArrayList;
+import java.lang.String;
+
 public class Dictionary {
     final String[] words = {"test", "android"};
+    public String wordfilter;
+    public String finalWord = "";
 
-    public void filter(String wordinput){
-        String[] filteredwords[];
-        for (String word : words) {
-            if (word.matches(wordinput + "(.*)")) {
-                filteredwords[].add(word);
+    public ArrayList<String> filterlist = new ArrayList<String>();
+
+
+    public void filter(String wordinput) {
+
+
+        if (filterlist.size() > 0) {
+            for (String word : filterlist) {
+                ArrayList<String> templist = new ArrayList<String>();
+                if (word.matches(wordinput + "(.*)")) {
+                    templist.add(word);
+                }
+                filterlist = templist;
             }
         }
-        count(filteredwords);
+
+        else {
+            for (String word : words) {
+                if (word.matches(wordinput + "(.*)")) {
+                    filterlist.add(word);
+
+                }
+            }
+        }
     }
 
-    public void count(filteredwords) {
-        int wordCount = filteredwords.size();
-        if (wourdCount == 1) {
-            result();
-        }
-        else {
-            return wordCount;
-        }
+    public void count(ArrayList<String> filterlist) {
+        int wordamount = filterlist.size();
+        return wordamount;
     }
 
     public void result() {
-        finalWord = filteredwords.get(0);
-        return finalWord;
+        int wordamount = count(filterlist);
+        if (wordamount == 1) {
+            String finalWord = filterlist.get(0);
+            return finalWord;
+        }
     }
 
-    public reset() {
+    public void reset() {
         filter("");
+    }
+
+
+}
+
+public class Game extends Dictionary {
+
+    public void guess(){
+        filter(wordinput);
+}
+
+    public void turn(){
+        //Sharedprefs opzoeken
+    }
+
+    public void ended() {
+        result();
+        if (finalWord != ""){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public void winner() {
+        boolean end = ended();
+        if (end == true){
+            //Sharedprefs opzoeken
+        }
+
+
     }
 
 
